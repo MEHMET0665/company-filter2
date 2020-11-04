8b34bb8ae933f840a9de15de758e29881e994c8e
Memet <meavci@gmail.com>
2020-11-04 10:00:04 -0500

    aba

---
+++ script.js
@@ -0,0 +1,26 @@
+const companies= [
+  {name: "Company One", category: "Finance", start: 1981, end: 2004},
+  {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+  {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+  {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+];
+
+// 1. step
+
+//for (i in companies ){
+
+ // console.log(companies[i])
+//}
+// 2.step
+//console.log(companies.forEach)
+
+
+//companies.sort(a=> a.start)
+
+const reducer = (a, b) => a + (b.end - b.start);
+console.log(companies.reduce(reducer,0));
